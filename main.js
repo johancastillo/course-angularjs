570207fce20cf1efab246c4553f460b8355e989b
Johan Castillo <jcjohan2707@gmail.com>
2021-12-03 16:42:21 -0400

    Two way data binding and ng-model

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-let app = angular.module("MyApp", ["ngResource"])
+let app = angular.module("MyApp", [])
 
 
 app.controller("FirstController", ($scope) => {
     $scope.name = "Jc Johan";
+    $scope.newComment = {};
     $scope.comments = [
         {
             description: "All fine all right",
@@ -12,7 +13,12 @@ app.controller("FirstController", ($scope) => {
             description: "Buenas tardes",
             username: "Jc Johan"
         }
-    ]
+    ];
+
+    $scope.addComment = () => {
+        $scope.comments.push($scope.newComment)
+        $scope.newComment = {}
+    }
 
 })
     
